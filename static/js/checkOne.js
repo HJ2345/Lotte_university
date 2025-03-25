0430fde621609ba2a59811a52f8cac4c166d2d28
HJ2345 <139225870+HJ2345@users.noreply.github.com>
2025-03-25 17:16:29 +0900

    Delete static directory

--- static/js/checkOne.js
+++
@@ -1,7 +0,0 @@
-function checkOnlyOne(element) {
-    const checkboxes = document.getElementsByClassName("checked");
-    Array.from(checkboxes).forEach((cb) => {
-        cb.checked = false;
-    });
-    element.checked = true;
-}
