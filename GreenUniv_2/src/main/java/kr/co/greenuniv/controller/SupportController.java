daaf9c6fe4426bb703136bea11ca360fcdc131c9
Gyeol00 <gyeol000726@gmail.com>
2025-03-26 17:48:37 +0900

    add all

---
+++ GreenUniv_2/src/main/java/kr/co/greenuniv/controller/SupportController.java
@@ -0,0 +1,14 @@
+package kr.co.greenuniv.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SupportController {
+
+    @GetMapping("/support/lectureRegister")
+    public String lectureRegister() {
+        return "support/lectureRegister";
+    }
+
+}
