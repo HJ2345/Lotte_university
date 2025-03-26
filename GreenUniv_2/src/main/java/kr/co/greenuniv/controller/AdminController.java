41e81ce33e26f919db76549becc714c66afa4d8d
진우 <128763219+Jinwoooooooo@users.noreply.github.com>
2025-03-26 15:43:39 +0900

    Done: 관리자 페이지 컨트롤러
    컨트롤러 완료 및 각 페이지에 필요한 링크 등 피그마에 새로 추가된 요소 추가 완료

--- GreenUniv_2/src/main/java/kr/co/greenuniv/controller/AdminController.java
+++ GreenUniv_2/src/main/java/kr/co/greenuniv/controller/AdminController.java
@@ -6,8 +6,58 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AdminController {
 
-    @GetMapping("/admin/adminMain")
+    @GetMapping("admin/adminMain")
     public String index(){
-        return "/admin/adminMain";
+        return "admin/adminMain";
+    }
+
+    @GetMapping("admin/courseStatus")
+    public String courseStatus(){
+        return "admin/courseStatus";
+    }
+
+    @GetMapping("admin/departmentList")
+    public String deptList(){
+        return "admin/departmentList";
+    }
+
+    @GetMapping("admin/eduStatus")
+    public String eduStatus(){
+        return "admin/eduStatus";
+    }
+
+    @GetMapping("admin/facultyList")
+    public String facultyList(){
+        return "admin/facultyList";
+    }
+
+    @GetMapping("admin/lecEnrollment")
+    public String lecEnrollment(){
+        return "admin/lecEnrollment";
+    }
+
+    @GetMapping("admin/lectureList")
+    public String lectureList(){
+        return "admin/lectureList";
+    }
+
+    @GetMapping("admin/profEnrollment")
+    public String profEnrollment(){
+        return "admin/profEnrollment";
+    }
+
+    @GetMapping("admin/stdEnrollment")
+    public String stdEnrollment(){
+        return "admin/stdEnrollment";
+    }
+
+    @GetMapping("admin/studentList")
+    public String studentList(){
+        return "admin/studentList";
+    }
+
+    @GetMapping("admin/univDeptEnrollment")
+    public String univDeptEnrollment(){
+        return "admin/univDeptEnrollment";
     }
 }
