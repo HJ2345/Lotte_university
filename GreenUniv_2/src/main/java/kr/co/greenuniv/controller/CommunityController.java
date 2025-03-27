9fbc1304eec782b5a3268ba453988834ec347523
euns.lee77 <euns.lee77@gmail.com>
2025-03-27 13:56:52 +0900

    Controller, html

---
+++ GreenUniv_2/src/main/java/kr/co/greenuniv/controller/CommunityController.java
@@ -0,0 +1,44 @@
+package kr.co.greenuniv.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CommunityController {
+
+    @GetMapping("/community/community_announcement")
+    public String community_announcement(){
+
+        return "/community/community_announcement";
+    }
+
+    @GetMapping("/community/Einformation")
+    public String Einformation(){
+
+        return "/community/Einformation";
+    }
+
+    @GetMapping("/community/Fboard")
+    public String Fboard(){
+
+        return "/community/Fboard";
+    }
+
+    @GetMapping("/community/news")
+    public String news(){
+
+        return "/community/news";
+    }
+
+    @GetMapping("/community/qna")
+    public String qna(){
+
+        return "/community/qna";
+    }
+
+    @GetMapping("/community/reference")
+    public String reference(){
+
+        return "/community/reference";
+    }
+}
