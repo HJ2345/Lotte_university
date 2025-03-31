58097692c630482dea31b4c03ab3c973119b15c8
beermoon <Feathermoon0318@gmail.com>
2025-03-31 09:21:45 +0900

    add choi

--- GreenUniv_2/src/main/java/kr/co/greenuniv/controller/AdminController.java
+++ GreenUniv_2/src/main/java/kr/co/greenuniv/controller/AdminController.java
@@ -1,10 +1,25 @@
 package kr.co.greenuniv.controller;
 
+import kr.co.greenuniv.dto.StudentDTO;
+import kr.co.greenuniv.service.StudentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import static java.rmi.server.LogStream.log;
+
+
+@Slf4j
 @Controller
 public class AdminController {
+
+    @Autowired
+    private StudentService studentService;  // 학생등록 service
+
 
     @GetMapping("admin/adminMain")
     public String index(){
@@ -47,9 +62,27 @@ public class AdminController {
     }
 
     @GetMapping("admin/stdEnrollment")
-    public String stdEnrollment(){
+    public String stdEnrollment(Model model){
+        model.addAttribute("studentDto", new StudentDTO());
         return "admin/stdEnrollment";
+
     }
+
+    @PostMapping("/admin/stdEnrollment")
+    public String savestdEnrollment(@ModelAttribute StudentDTO studentDto) {
+
+
+        log.info("입학구분: {}", studentDto.getAdmissionType());
+        log.info("입학학기: {}", studentDto.getAdmissionGrade());
+        log.info("입학학년: {}", studentDto.getAdmissionSemester());
+
+
+        studentService.save(studentDto);
+
+
+        return "redirect:/admin/stdEnrollment";
+    }
+
 
     @GetMapping("admin/studentList")
     public String studentList(){
---
+++ GreenUniv_2/src/main/java/kr/co/greenuniv/entity/Student.java
@@ -0,0 +1,57 @@
+package kr.co.greenuniv.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Table(name="student")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "S_num")
+    private int S_num;
+
+    private String S_name;
+    private String S_Ename;
+    private String gender;
+    private String S_addr1;
+    private String S_addr2;
+    private String S_nation;
+    private String S_hp;
+    private String S_email;
+    private String S_residentId;
+
+    private int admissionYear;
+    private String admissionType;
+    private String admissionGrade;
+    private String admissionSemester;
+    private int graduationYear;
+
+
+
+
+
+//    private int S_adDate;
+//    private int S_gradDate;
+//    private String S_adstatus;
+//    private String S_lesson;
+//    private String S_spec;
+//    private String S_grad;
+//    private String S_term;
+//    private String S_advisor;
+
+//    @ManyToOne
+//    @JoinColumn(name = "professor_P_num")
+//    private Professor professor;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "department_student")
+//    private Department department;
+
+
+}
