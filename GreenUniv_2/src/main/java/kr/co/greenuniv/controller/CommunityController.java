b0b7c24bda7722b6fbc7f0e56689d169fc43367f
Gyeol00 <gyeol000726@gmail.com>
2025-04-02 17:41:20 +0900

    add all

--- GreenUniv_2/src/main/java/kr/co/greenuniv/controller/CommunityController.java
+++ GreenUniv_2/src/main/java/kr/co/greenuniv/controller/CommunityController.java
@@ -1,13 +1,16 @@
 package kr.co.greenuniv.controller;
 
+import kr.co.greenuniv.dto.CommunityInfoDTO;
 import kr.co.greenuniv.dto.PageRequestDTO;
 import kr.co.greenuniv.dto.PageResponseDTO;
 import kr.co.greenuniv.service.CommunityService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @RequiredArgsConstructor
 @Controller
 public class CommunityController {
@@ -22,6 +25,7 @@ public class CommunityController {
 
         // 전체 글 조회 서비스 호출(Mybatis)
         model.addAttribute(pageResponseDTO);
+        log.info(pageResponseDTO.toString());
 
         return "/community/CommunityInfo";
     }
